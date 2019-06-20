1942092292a988ca556f5f7bee5d2728fbce991c
Hongtu Zang <zanghongtu2006@163.com>
2019-06-20 12:37:10 +0800

    调整依赖

--- service-demo/src/test/java/com/zanghongtu/servicedemo/GenerateDBSourceCode.java
+++ service-demo/src/test/java/com/zanghongtu/servicedemo/GenerateDBSourceCode.java
@@ -1,19 +1,11 @@
 package com.zanghongtu.servicedemo;
 
 import com.zanghongtu.database.Dao2POJO;
-import com.zanghongtu.servicedemo.testonly.model.Warning;
-import com.zanghongtu.servicedemo.testonly.model.WarningRules;
-import com.zanghongtu.servicedemo.testonly.repository.WarningRepository;
-import com.zanghongtu.servicedemo.testonly.repository.WarningRulesRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,14 +19,4 @@ public class GenerateDBSourceCode {
         dao2POJO.generateCodes();
     }
 
-    @Autowired
-    private WarningRulesRepository repository;
-
-    @Test
-    public void testCreate() {
-        Map<String, String> conditions = new HashMap<>();
-        conditions.put("tagName", "SWC");
-        List<WarningRules> models = repository.listAll();
-        System.out.println("***" +models.size() + "***" + models);
-    }
 }
